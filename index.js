cd6d56c132f5b3a36f3e834d83c12393ca9e1657
Gunnish Mehta <gunnishmehta2003@gmail.com>
2023-12-11 13:47:52 +0530

    Changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const bodyParser = require("body-parser");
 const pg = require("pg");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 let loggedIn = false;
 let user_data=[];
 let logout = 'Logout';
